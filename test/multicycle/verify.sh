cdf260c4378418e6a1b1d366aea46ed45439b546
HobbitQia <861394755@qq.com>
2025-04-25 08:16:06 +0000

    test

--- test/multicycle/verify.sh
+++ test/multicycle/verify.sh
@@ -5,10 +5,10 @@ inclusive_dfg_count=$(grep -o 'id' inclusive_dfg.json | wc -l)
 if [ "$exclusive_dfg_count" -neq 11 ]; then
     echo "Multi-Cycle Test Failed! The count of DFG nodes in the exclusive strategy shoule be 11, but got $exclusive_dfg_count."
     exit 1
-if [ "$inclusive_dfg_count" -neq 11 ]; then
+else if [ "$inclusive_dfg_count" -neq 11 ]; then
     echo "Multi-Cycle Test Failed! The count of DFG nodes in the inclusive strategy shoule be 11, but got $inclusive_dfg_count."
     exit 1
-if [ "$distributed_dfg_count" -neq 16 ]; then
+else if [ "$distributed_dfg_count" -neq 16 ]; then
     echo "Multi-Cycle Test Pass! The count of DFG nodes in the distributed strategy shoule be 16, but got $distributed_dfg_count."
     exit 1
 else
