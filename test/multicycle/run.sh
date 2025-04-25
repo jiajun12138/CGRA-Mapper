2fe02f65c2f4370cbdb057d839c7d6ba7772681b
HobbitQia <861394755@qq.com>
2025-04-25 08:36:24 +0000

    test

--- test/multicycle/run.sh
+++ test/multicycle/run.sh
@@ -4,6 +4,6 @@ mv dfg.json exclusive_dfg.json
 cat param_distributed.json > param.json 
 opt-12 -load ../../build/src/libmapperPass.so -mapperPass multicycle_test.bc
 mv dfg.json distributed_dfg.json
-# cat param_inclusive.json > param.json 
-# opt-12 -load ../../build/src/libmapperPass.so -mapperPass multicycle_test.bc
-# mv dfg.json inclusive_dfg.json
+cat param_inclusive.json > param.json 
+opt-12 -load ../../build/src/libmapperPass.so -mapperPass multicycle_test.bc
+mv dfg.json inclusive_dfg.json
